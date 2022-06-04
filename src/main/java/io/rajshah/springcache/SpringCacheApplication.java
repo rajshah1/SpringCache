dcbed633304d4bb76a533f8716171f9dc0fb9166
rajshah1 <raj.shah@infosys.com>
2022-06-03 20:26:27 -0400

    :arrow_up: Start Project

---
+++ src/main/java/io/rajshah/springcache/SpringCacheApplication.java
@@ -0,0 +1,13 @@
+package io.rajshah.springcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheApplication.class, args);
+    }
+
+}
